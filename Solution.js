51bf3bb5a2a71475a191b3b90b66b53031e814c0
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-12-02 20:43:12 +0100

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,32 @@
+
+/**
+ * @param {number[]} input
+ * @return {number}
+ */
+var minMirrorPairDistance = function (input) {
+    const NOT_FOUND = -1;
+    let minMirrorPairDistance = Number.MAX_SAFE_INTEGER;
+    const reversedValueToLatestIndex = new Map();
+
+    for (let i = 0; i < input.length; ++i) {
+        if (reversedValueToLatestIndex.has(input[i])) {
+            minMirrorPairDistance = Math.min(minMirrorPairDistance, i - reversedValueToLatestIndex.get(input[i]));
+        }
+        reversedValueToLatestIndex.set(reverseValue(input[i]), i);
+    }
+    return minMirrorPairDistance !== Number.MAX_SAFE_INTEGER ? minMirrorPairDistance : NOT_FOUND;
+};
+
+/**
+ * @param {number} value
+ * @return {number}
+ */
+function reverseValue(value) {
+    let reversedValue = 0;
+    while (value > 0) {
+        const digit = value % 10;
+        reversedValue = reversedValue * 10 + digit;
+        value = Math.floor(value / 10);
+    }
+    return reversedValue;
+}
